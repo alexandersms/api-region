9c706a3f7de58e0a5ad53c105eeb10754fa7b4c9
Alex Simisi <smsxander@gmail.com>
2019-07-31 15:52:21 +0200

    Création de la liste des départements par région

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,19 +5,57 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ApiController extends AbstractController
 {
     /**
-     * @Route("/api", name="api")
+     * @Route("/", name="api")
      */
     public function index(SerializerInterface $serialize)
     {
         $regions = file_get_contents('https://geo.api.gouv.fr/regions');
-        $lesRegions = $serialize->decode($regions, 'json');
-        //dd($lesRegions);
+        // Methode Decode
+        //$lesRegions = $serialize->decode($regions, 'json');
+
+        // Methode Denormalize
+        //$objRegions = $serialize->denormalize($lesRegions, 'App\Entity\Region[]');
+
+        // Deserialisation, qui fait du decode et du denormalize en même temps
+        $deserRegions = $serialize->deserialize($regions, 'App\Entity\Region[]', 'json');
+
+        //dd($objRegions);
         return $this->render('api/index.html.twig', [
-            'lesRegions' => $lesRegions
+            'lesRegions' => $deserRegions
+        ]);
+    }
+
+
+    /**
+     * @Route("/listedpt", name="api_listedpt")
+     */
+    public function listedpt(SerializerInterface $serialize, Request $request)
+    {
+        // Je recupère la région selectionnée dans le formulaire
+        $codeRegion = $request->query->get('region');
+        // Je recupère les région
+        $regions = file_get_contents('https://geo.api.gouv.fr/regions');
+        // Deserialisation des régions
+        $deserRegions = $serialize->deserialize($regions, 'App\Entity\Region[]', 'json');
+
+        // Je récupère la liste des départements
+        if ($codeRegion == null || $codeRegion == "Toutes") {
+            $mesDpt = file_get_contents('https://geo.api.gouv.fr/departements');
+        } else {
+            $mesDpt = file_get_contents('https://geo.api.gouv.fr/regions/' . $codeRegion . '/departements');
+        }
+
+        //décodage du format json en tableau
+        $mesDpt = $serialize->decode($mesDpt, 'json');
+
+        return $this->render('api/listeDpt.html.twig', [
+            'lesRegions' => $deserRegions,
+            'mesDpt' => $mesDpt
         ]);
     }
 }
---
+++ src/Entity/Region.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+
+/**
+ * 
+ */
+class Region
+{
+    /**
+     *
+     */
+    private $id;
+
+    /**
+     * 
+     */
+    private $code;
+
+    /**
+     *
+     */
+    private $nom;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+}
